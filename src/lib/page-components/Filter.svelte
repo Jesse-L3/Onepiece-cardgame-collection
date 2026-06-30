fc0ab924c2a31a1645718cc518622b8ef4236520
J3SS3HVA <jesse.leeuwerink@hva.nl>
2026-06-30 14:02:36 +0200

    styling changes

--- src/lib/page-components/Filter.svelte
+++ src/lib/page-components/Filter.svelte
@@ -2,8 +2,12 @@
     let { name = '' } = $props();
 </script>
 
-<form action="/" method="get">
+<form action="/" method="get" class="flex items-center justify-center">
     <div class="search">
-        <input type="search" id="search" name="n" value={name} />
+        <input class="text-[var(--text-color-dark)] rounded-lg" type="search" id="search" name="n" value={name} placeholder="Search by name"/>
     </div>
 </form>
+
+<style>
+
+</style>
